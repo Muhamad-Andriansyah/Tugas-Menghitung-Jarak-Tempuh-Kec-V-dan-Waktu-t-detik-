225dc229f459b671299d8b53d22e53be05d448b7
Muhamad-Andriansyah <45659337+Muhamad-Andriansyah@users.noreply.github.com>
2019-01-03 13:15:10 +0700

    Add files via upload

---
+++ Tugas Menghitung Jarak Tempuh Kec V dan Waktu t (detik).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "MENGHITUNG JARAK TEMPUH DENGAN KEC V DAN WAKTU - MUHAMAD ANDRIANSYAH (NIM 311810297) TI-18 B2" << endl;
+
+    int v, t, s;
+    cout<<"---menghitung jarak tempuh perjalanan---";
+
+    cout<<"/n menghitung kecepatan M/detik= ";
+    cin>> v;
+    cout<<"/n masukan waktu yang di tempuh (detik) =";
+    cin>> t;
+
+    //rumus
+    s = v * t;
+    cout<<"jarak yang ingin ditempuh adalah = "<<5<<"meter";
+    return 0;
+}
